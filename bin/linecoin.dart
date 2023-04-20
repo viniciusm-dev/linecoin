0334694b10f8b3d1e57755fa0571834c481031c3
Vinicius Mesquita <aiunpa8@amway.com>
2023-04-20 15:58:11 -0300

    Dart Package Created

---
+++ bin/linecoin.dart
@@ -0,0 +1,5 @@
+import 'package:linecoin/linecoin.dart' as linecoin;
+
+void main(List<String> arguments) {
+  print('Hello world: ${linecoin.calculate()}!');
+}
---
+++ test/linecoin_test.dart
@@ -0,0 +1,8 @@
+import 'package:linecoin/linecoin.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
