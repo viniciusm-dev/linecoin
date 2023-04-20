a84a1c6ab49a98d9f8d9f171975d83b35c3ec070
Vinicius Mesquita <aiunpa8@amway.com>
2023-04-20 16:24:58 -0300

    Bosun initially implemented

--- bin/linecoin.dart
+++ bin/linecoin.dart
@@ -1,5 +1,28 @@
 import 'package:linecoin/linecoin.dart' as linecoin;
+import 'package:bosun/bosun.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${linecoin.calculate()}!');
+class AppCmd extends Command {
+  AppCmd() : super(command: 'app', description: 'run as an app shell');
+
+  @override
+  void run(List<String> args, Map<String, dynamic> flags) {
+    print("in the app command callback");
+  }
+}
+
+class RunCmd extends Command {
+  RunCmd()
+      : super(
+            command: 'run',
+            description: 'Run a command in a shell',
+            subcommands: [AppCmd()]);
+
+  @override
+  void run(List<String> args, Map<String, dynamic> flags) {
+    print("in the run command callback");
+  }
+}
+
+void main(List<String> args) {
+  execute(BosunCommand('donker', subcommands: [RunCmd()]), args);
 }
